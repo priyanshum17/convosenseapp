934541f90ca40f3b7ae8d200f7bc9a945408ef19
Priyanshu Mehta <priyanshum1709@gmail.com>
2025-07-10 05:30:50 +0000

    what's the reason for this empt space

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -7,7 +7,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <div className="flex h-screen bg-background text-foreground">
         <UserList />
-        <main className="flex-1 flex flex-col relative min-w-0">
+        <main className="flex-1 flex flex-col relative min-w-0 h-screen">
             <div className="absolute top-4 left-4 z-20 md:hidden">
                <SidebarTrigger />
             </div>
